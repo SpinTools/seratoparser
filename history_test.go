9ec47c466ecbd06cfc1f712a0b2d3405183cab92
Stoyvo <stoyanovski.development@gmail.com>
2021-05-30 00:11:44 -0400

    Remove log from tests

--- history_test.go
+++ history_test.go
@@ -13,8 +13,7 @@ func TestReadHistorySession(t *testing.T) {
 		t.Errorf("GetHistorySessions() = %q, want %q", strconv.Itoa(len(sessions)), ">0")
 		log.Println(sessions)
 	}
-
-	log.Println(sessions[0].Name())
+	
 	historyEntities := p.ReadHistorySession(sessions[0].Name())
 	if len(historyEntities) == 0 {
 		t.Errorf("ReadHistorySession() = %q, want %q", strconv.Itoa(len(historyEntities)), ">0")
